eaa0a30d76f97f96eca76f05cb209b6d94ed89f7
bastien <mokuhazushi.programming@gmail.com>
2017-01-15 18:22:15 +0100

    Emitter protocol implementation

--- src/EmitterImpl.java
+++ src/EmitterImpl.java
@@ -1,32 +1,73 @@
 
+import peersim.config.Configuration;
+import peersim.core.Network;
 import peersim.core.Node;
+import peersim.edsim.EDSimulator;
 
 /**
  * @author mokuhazushi
  */
 public class EmitterImpl implements Emitter {
+    private static final String PAR_SCOPE = "scope";
+    private static final String PAR_LATENCY = "latency";
+    private static final String PAR_POSITIONPID = "positionprotocol";
+    private static final String PAR_ELECTIONPID = "electionprotocol";
+    
+    private int position_pid;
+    private int election_pid;
+    private int scope;
+    private int latency;
 
-    public EmitterImpl(String index) {
+    public EmitterImpl(String prefix) {
+        this.position_pid = Configuration.getPid(prefix+"."+PAR_POSITIONPID);
+        election_pid = Configuration.getPid(prefix+"."+PAR_ELECTIONPID);
+        this.scope = Configuration.getInt(prefix+"."+PAR_SCOPE);
+        this.latency = Configuration.getInt(prefix+"."+PAR_LATENCY);
+    }
+
+    public EmitterImpl(int scope, int latency) {
+        this.scope = scope;
+        this.latency = latency;
     }
 
     @Override
     public void emit(Node host, Message msg) {
-        return;
+        Node myself;
+        for (int i=0; i<Network.size(); i++) {
+            Node n = Network.get(i);
+            if (n.getID() == host.getID()) continue;
+            
+            PositionProtocol hostPos = (PositionProtocol) host.getProtocol(position_pid);
+            PositionProtocol nPos = (PositionProtocol) n.getProtocol(position_pid);
+            if (isInRadius(hostPos, nPos)) {
+                //EMIT MESSAGE
+                EDSimulator.add(0, msg, n, election_pid);
+            }
+        }
+    }
+    
+    private boolean isInRadius(PositionProtocol host, PositionProtocol n) {
+        double hostX = host.getX();
+        double hostY = host.getY();
+        double nX = host.getX();
+        double nY = host.getY();
+        
+        return Math.pow(hostX-nX, 2) + Math.pow(hostY-nY, 2) < Math.pow(scope, 2);
     }
 
     @Override
     public int getLatency() {
-        return 0;
+        return this.latency;
     }
 
     @Override
     public int getScope() {
-        return 0;
+        return this.scope;
     }
     
     @Override
     public Object clone() {
-        return new EmitterImpl("");
+        return new EmitterImpl(scope, latency);
     }
 
     
